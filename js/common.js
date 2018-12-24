aee3a3eb60e82dd83d66f818cea981b21397b2e5
WEBZLG <webzlg@163.com>
2018-12-24 17:01:11 +0800

    LETU

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function(){
+	menuBtn($(".menu-btn"))
+	addBg($(".menu-list"))
+//	首页导航按钮
+function menuBtn(dom){
+	dom.on("click",function(){
+		$(this).next().toggle(200)
+	})
+}
+//导航添加背景色
+function addBg(dom){
+	dom.find("li").on("click",function(){
+		$(this).addClass("active").siblings().removeClass("active")
+	})
+}
+})
